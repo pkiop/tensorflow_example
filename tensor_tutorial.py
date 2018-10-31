2120c19b6ad6e4820ea3af45a1ebf1880bbca8e8
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2018-11-01 00:03:09 +0900

    Update tensor_tutorial.py

--- tensor_tutorial.py
+++ tensor_tutorial.py
@@ -20,6 +20,7 @@ y = tf.nn.softmax(tf.matmul(x,W) + b)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
+
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 
