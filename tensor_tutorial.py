b8e79b24b93183b6e3f499e54c7e593277f59a38
pkiop@naver.com <pkiop@naver.com>
2018-10-31 09:50:21 -0700

    .

--- tensor_tutorial.py
+++ tensor_tutorial.py
@@ -6,4 +6,20 @@ import tensorflow as tf
 
 print tf.convert_to_tensor(mnist.train.images).get_shape()
 
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
 
