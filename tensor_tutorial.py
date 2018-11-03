5dae11647e8ed444c037c2b05be29548967efbc9
pkiop@naver.com <pkiop@naver.com>
2018-11-03 03:51:15 -0700

    test

--- tensor_tutorial.py
+++ tensor_tutorial.py
@@ -21,5 +21,16 @@ y = tf.nn.softmax(tf.matmul(x,W) + b)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+print "do"
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x:batch[0], y_ : batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
 
 
