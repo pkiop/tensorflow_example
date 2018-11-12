1235aff84924f38e0be7321e691821041dd1bd9c
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2018-11-12 22:01:21 +0900

    Update alloftf/alloftf3.py

--- alloftf/alloftf3.py
+++ alloftf/alloftf3.py
@@ -1,29 +1,46 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-sess = tf.Session()
+x_data = [1,2,3]
+y_data = [1,2,3]
 
 W = tf.Variable(tf.random_normal([1]), name = 'weight')
-b = tf.Variable(tf.random_normal([1]), name = 'bias')
-X = tf.placeholder(tf.float32, shape=[None])# None => 어떤 값이든 와도 된다. 
-Y = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32)
 
-hypothesis = X * W + b
+hypothesis = X * W
 
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
-#magic
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
-train = optimizer.minimize(cost)
-
 sess = tf.Session()
+
 sess.run(tf.global_variables_initializer())
 
-for step in range(2001):
-    cost_val, W_val, b_val, _ =sess.run([cost, W, b, train], feed_dict={X:[1,2,3,4,5], Y:[2.1,3.1,4.1,5.1,6.1]})
-    if step % 20 == 0:
-        print(step, cost_val, W_val, b_val)
+#W_val = []
+#cost_val = []
+#for i in range(-30, 50):
+#        feed_W = i * 0.1
+#        curr_cost, curr_W = sess.run([cost, W], feed_dict={W: feed_W})
+#        W_val.append(curr_W)
+#        cost_val.append(curr_cost)
 
+#plt.plot(W_val, cost_val)
+#plt.show()
 
-#Testing our model
+# learning_rate = 0.1
+# gradient = tf.reduce_mean((W*X - Y) * X)
+# descent = W - learning_rate * gradient
+# update = W.assign(descent)
 
-print(sess.run(hypothesis, feed_dict={X: [5]}))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+train = optimizer.minimize(cost)
+
+for step in range(21):
+        sess.run(train, feed_dict={X:x_data, Y:y_data})
+        print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+
+gvs = optimizer.compute_gradients(cost) # 이 cost 에 맞는 gradient 를 돌려줘
+#원하는대로 수정  
+apply_gradients = optimizer.apply_gradients(gvs) # 수정해서 적용 가능 
+
