f41bf38a81f324cf67c49b42ad9c6c884af6a22b
pkiop <vivezdv@gmail.com>
2018-11-14 22:06:39 +0900

    Update tensor_tutorial.py

--- tensor_tutorial.py
+++ tensor_tutorial.py
@@ -5,6 +5,28 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
 import tensorflow as tf
 
 print(tf.convert_to_tensor(mnist.train.images).get_shape())
+<<<<<<< HEAD
+=======
+
+sess = tf.InteractiveSession()
+
+def variable_summaries(var, name):
+    """Attach a lot of summaries to a Tensor."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean/' + name, mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+        tf.summary.scalar('stddev/' + name, stddev)
+        tf.summary.scalar('max/' + name, tf.reduce_max(var))
+        tf.summary.scalar('min/' + name, tf.reduce_min(var))
+        tf.summary.histogram(name, var)
+summary.scalarfrom tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+
+import tensorflow as tf
+print tf.convert_to_tensor(mnist.train.images).get_shape()
 
 sess = tf.InteractiveSession()
 
@@ -19,8 +41,86 @@ sess.run(tf.initialize_all_variables())
 y = tf.nn.softmax(tf.matmul(x,W) + b)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+for i in range(1000):
+batch = mnist.train.next_batch(50)
+train_step.run(feed_dict={x:batch[0], y_ : batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})summary.scalarfrom tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+
+import tensorflow as tf
+print tf.convert_to_tensor(mnist.train.images).get_shape()
+>>>>>>> c37fe0c2e4ca1aa62dcac54d91415d064b134370
+
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+for i in range(1000):
+batch = mnist.train.next_batch(50)
+train_step.run(feed_dict={x:batch[0], y_ : batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+variable_summaries(x, 'x')
+variable_summaries(y_, 'y_')
+variable_summaries(W, 'W')
+variable_summaries(b, 'b')
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
+<<<<<<< HEAD
 print("end")
+=======
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x:batch[0], y_ : batch[1]})
+    if i%100 == 0 : 
+        variable_summaries(x, 'x')
+        variable_summaries(y_, 'y_')
+        variable_summaries(W, 'W')
+        variable_summaries(b, 'b')
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+merged = tf.summary.merge_all()
+train_writer = tf.summary.FileWriter('./summary', sess.graph)
+test_writer = tf.summary.FileWriter('./test')
+
+
+
+>>>>>>> c37fe0c2e4ca1aa62dcac54d91415d064b134370
 
