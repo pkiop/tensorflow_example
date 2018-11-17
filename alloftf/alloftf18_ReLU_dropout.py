55ccc74251a7fe2a03154f5f2af37a6e74d1fac3
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2018-11-17 11:40:03 +0900

    '.'

--- alloftf/alloftf18_ReLU_dropout.py
+++ alloftf/alloftf18_ReLU_dropout.py
@@ -14,3 +14,8 @@ b3 = tf.Variable(tf.zeros([1]), name = 'Bias3')
 L2 = tf.sigmoid(tf.matmul(X, W1) + b1)
 L3 = tf.sigmoid(tf.matmul(L2, W2) + b2)
 hypothesis = tf.sigmoid(tf.matmul(L3, W3) + b3)
+
+
+dropout_rate = tf.placeholder("float")
+_L1 = tf.nn.relu(tf.add(tf.matmul(X,W1), B1))
+L1 = tf.nn.dropout(_L1, dropout_rate)
